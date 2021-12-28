3605f0d5a18550bd7f2f3f2213f1d5a431b34631
mahajansuraj <mahajansuraj.com@gmail.com>
2021-12-28 23:55:26 +0530

    minor change

--- src/main/java/com/controller/ContactRestController.java
+++ src/main/java/com/controller/ContactRestController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bindings.ContactForm;
 import com.service.ContactService;
 
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ContactRestController {
 
